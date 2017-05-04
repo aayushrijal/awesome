7e194f142028e9e8111df30b696dd3b192b4d2ec
Aayush Rijal <reizal.ayz@gmail.com>
2017-05-04 13:02:51 +0545

    Image gallery integrated and images uploaded

--- config/body.php
+++ config/body.php
@@ -3,13 +3,9 @@
 return [
     'main_name'            => 'ARYA STARK',
     'profession'           => 'GRAPHIC DESIGNER',
-    'personal_description' => 'My name is Ellen and I specialize in icon and logo design. As I\'ve developed my
-    skills, my passion for flat illustration has grown. I am drawn toward modern art and a minimalist approach. There
-     is a beauty that comes from simplicity and a welcome challenge in designing it. I have a background in branding
-     and presentation design, and continue to be inspired by my community here in the San Francisco Bay Area. I am
-     currently working as a designer for Duarte, Inc. where I tackle the demand for innovative design in a field that
-      is constantly evolving. I dabble with calligraphy and enjoy putting together stellar dinner parties with
-      friends. I have a passion for running, travel as much as I can, and obsess over French Bulldogs. I am a
-      designer, an optimist, an explorer, and a modern woman looking to take on the world one challenge at a time.',
+    'personal_description' => 'My name is Roshni Adhikari. As I\'ve developed my skills, my passion for modelling has
+    grown. I am drawn toward modern aesthetics of clothing and fashion. There is a beauty that comes from simplicity
+    and a welcome challenge in accepting it. I have a background in dentistry, and continue to be inspired by my
+    community here in the Nepal. I am currently studying to become Dental Surgeon.',
     'get_in_touch'         => 'Send me a little information about what you\'re looking for and I\'ll be in touch!',
 ];
--- resources/views/home/gallery.blade.php
+++ resources/views/home/gallery.blade.php
@@ -1,8 +1,130 @@
 @extends('layouts.app')
 
+@section('style')
+	<link rel="stylesheet" type="text/css" href="{{asset('css/gridgallery/demo.css')}}"/>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/gridgallery/component.css')}}"/>
+	<script src="{{asset('js/gridgallery/modernizr.custom.js')}}"></script>
+@endsection
+
 @section('content')
-	<h1>Gallery</h1>
+	<div class="gallery-wrapper">
+
+	</div>
+	<div class="container">
+		<div id="grid-gallery" class="grid-gallery">
+			<section class="grid-wrap">
+				<ul class="grid">
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img01"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img02"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img03"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img04"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img05"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img06"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img01"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img02"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img03"/>
+						</figure>
+					</li>
+				</ul>
+			</section>
+			<section class="slideshow">
+				<ul>
+					<li>
+						<figure>
+							<img src="{{asset('images/dummy.jpg')}}" alt="img01"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img02"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img03"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img04"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img05"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img06"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img01"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img02"/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<img src="{{ asset('images/dummy.jpg')}}" alt="img03"/>
+						</figure>
+					</li>
+				</ul>
+				<nav>
+					<span class="icon nav-prev"></span>
+					<span class="icon nav-next"></span>
+					<span class="icon nav-close"></span>
+				</nav>
+			</section>
+		</div>
+	</div>
 @endsection
 
 @section('script')
+	<script src="{{('js/gridgallery/imagesloaded.pkgd.min.js')}}"></script>
+	<script src="{{asset('js/gridgallery/masonry.pkgd.min.js')}}"></script>
+	<script src="{{asset('js/gridgallery/classie.js')}}"></script>
+	<script src="{{asset('js/gridgallery/cbpGridGallery.js')}}"></script>
+	<script>
+		new CBPGridGallery(document.getElementById('grid-gallery'));
+	</script>
 @endsection
